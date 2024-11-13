476ef8d05b03164f308eea2a1e622a21a90a6f34
zhenghchen <zhenghchen2024@gmail.com>
2024-11-12 21:47:32 -0800

    Asked for Location permission

--- ratemypoo/lib/pages/map.dart
+++ ratemypoo/lib/pages/map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapWidget extends StatefulWidget {
   const MapWidget({super.key});
@@ -10,6 +11,8 @@ class MapWidget extends StatefulWidget {
 
 class _MapWidgetState extends State<MapWidget> {
 
+  Location _LocationController = new Location();
+
   static const LatLng OSU = LatLng(44.5618, -123.2823);
 
   @override
@@ -17,5 +20,29 @@ class _MapWidgetState extends State<MapWidget> {
     return const Scaffold(
       body: GoogleMap(initialCameraPosition: CameraPosition(target: OSU, zoom: 15 ))
       );
+  }
+
+  Future<void> getLocationTracking() async{
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _LocationController.serviceEnabled();
+    if(_serviceEnabled){
+
+      _serviceEnabled = await _LocationController.requestService();
+    }
+    else{
+      return;
+    }
+
+    _permissionGranted = await _LocationController.hasPermission();
+    if(_permissionGranted == PermissionStatus.denied){
+      _permissionGranted = await _LocationController.requestPermission();
+      if(_permissionGranted !=PermissionStatus.granted){
+        return;
+      }
+    }
+
   }
 }
