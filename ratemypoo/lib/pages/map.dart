6bcc7b5cb929e06ff316452be37f8d00e3ac820c
zhenghchen <zhenghchen2024@gmail.com>
2024-11-12 19:01:14 -0800

    Linked the Google Maps API into a statefull widget map page.

--- ratemypoo/lib/pages/map.dart
+++ ratemypoo/lib/pages/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapWidget extends StatefulWidget {
   const MapWidget({super.key});
@@ -9,10 +10,12 @@ class MapWidget extends StatefulWidget {
 
 class _MapWidgetState extends State<MapWidget> {
 
+  static const LatLng OSU = LatLng(44.5618, -123.2823);
+
   @override
   Widget build(BuildContext context) {
-    return  const Placeholder(
-      color: Colors.green,
-    );
+    return const Scaffold(
+      body: GoogleMap(initialCameraPosition: CameraPosition(target: OSU, zoom: 15 ))
+      );
   }
 }
